6cc26d1237467b05b2e377afdceb668e5d5f5b70
Ian Kosen <i@ikosenn.me>
2015-05-21 23:10:06 +0300

    Refistered the Snippet model in the admin module

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -5,7 +5,7 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 class SnippetSerializer(serializers.Serializer):
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style',)
     # pk = serializers.IntegerField(read_only=True)
     # title = serializers.CharField(required=False, allow_blank=True, max_length=100)
     # code = serializers.CharField(style={'base_template':'textarea.html'})
