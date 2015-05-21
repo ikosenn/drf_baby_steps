37aba9e881c53d32f86ccc7b61eaac3d631c757f
Ian Kosen <i@ikosenn.me>
2015-05-21 21:57:47 +0300

    Minor edits on the url modules

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^snippets/', include('snippets.url')),
+    url(r'^', include('snippets.urls')),
 ]
