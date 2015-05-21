5e24520eb7ed535e84db0d695604cb1cec7f5831
Ian Kosen <i@ikosenn.me>
2015-05-21 21:32:39 +0300

    Created a snippet app list view

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,3 +1,59 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from .models import Snippet
+from .serializers import SnippetSerializer
+
 # Create your views here.
+
+
+class JSONResponse(HttpResponse):
+
+    '''
+    A HttpREsponse that renders its contents to JSONResponse
+    '''
+
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+@csrf_exempt
+def snippet_list(request):
+    '''
+    List all code snippets, or create a new snippet
+    '''
+    if request.method == 'GET':
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many = True)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'POST':
+        data = JSONParser().parser(request)
+        serializer = SnippetSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data, status = 201)
+        return JSONResponse(serializer.errors, status=400)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
