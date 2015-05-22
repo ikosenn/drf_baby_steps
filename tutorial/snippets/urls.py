edba23ac149888b910ae1c0ede63c45bbc6948f1
Ian Kosen <i@ikosenn.me>
2015-05-22 11:29:26 +0300

    Edits on the snippet url

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^$', views.api_root),
     url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view(), name='snippet-highlight'),
     url(r'^snippets/$', views.SnippetList.as_view(), name = 'snippet-list'),
-    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetDetail.as_view(), name = 'snippet-detail'),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view(), name = 'snippet-detail'),
 
     url(r'^users/$', views.UserList.as_view(), name = 'user-list'),
     url(r'^users/(?P<pk>[0-9]+)/$',views.UserDetail.as_view(), name = 'user-detail'),
